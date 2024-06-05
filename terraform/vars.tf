c5d45eaf2ac1fa730d47dbb2fcecbfddceebaa8e
2310781001 <2310781001@fh-burgenland.at>
2024-06-05 22:52:48 +0200

    terraform

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,11 +1,11 @@
 variable "exo_secret" {
   type      = string
   sensitive = true
-  default   = 
+  default   = "goievOEff9BZf67cDjOXAmxpu_3RqkNh0SEIto6TKEE" 
 }
 
 variable "exo_key" {
   type      = string
   sensitive = true
-  default   = 
+  default   = "EXO481f7d3f65bd795012b23789"
 }
