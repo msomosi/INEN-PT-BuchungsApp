ebc5eaa30dbf1cb1da57d625c3f4e635ebac4473
Raul Cimpoeru <r.cimpoeru@student.tugraz.at>
2024-06-06 23:44:22 +0200

    Working version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,31 @@ locals {
   zone = "at-vie-2"
 }
 
+resource "aws_s3_bucket" "mein_bucket" {
+  bucket = "test1"  # Name des Buckets, wie bereits verwendet
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "Test Bucket Raul"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mein_bucket.id
+
+  block_public_acls   = true
+  ignore_public_acls  = true
+  block_public_policy = true
+  restrict_public_buckets = true
+}
+
 resource "exoscale_security_group" "my_security_group" {
-  name = "my-sks-cluster-sg"
+  name = "my-cluster-security-group"
 }
 
 resource "exoscale_security_group_rule" "kubelet" {
@@ -13,7 +36,6 @@ resource "exoscale_security_group_rule" "kubelet" {
   protocol          = "TCP"
   start_port        = 10250
   end_port          = 10250
-  # (beetwen worker nodes only)
   user_security_group_id = exoscale_security_group.my_security_group.id
 }
 
@@ -37,12 +59,11 @@ resource "exoscale_security_group_rule" "allow_all_egress" {
   cidr              = "0.0.0.0/0"
 }
 
-
 resource "exoscale_sks_cluster" "my_sks_cluster" {
   zone          = local.zone
   name          = "my-sks-cluster"
   cni           = "cilium"
-  service_level = "starter" //Einschalten bei der Übung ansonsten standartmäßig Pro -Version (kostet mehr)
+  service_level = "starter"
 }
 
 resource "exoscale_sks_nodepool" "my_sks_nodepool" {
@@ -51,19 +72,17 @@ resource "exoscale_sks_nodepool" "my_sks_nodepool" {
   name          = "my-sks-nodepool"
   instance_type = "standard.medium"
   size          = 3
-  security_group_ids = [
-    exoscale_security_group.my_security_group.id,
-  ]
+  security_group_ids = [exoscale_security_group.my_security_group.id]
 }
 
 resource "exoscale_sks_kubeconfig" "my_sks_kubeconfig" {
-  zone                  = local.zone
-  cluster_id            = exoscale_sks_cluster.my_sks_cluster.id
-  user                  = "kubernetes-admin"
-  groups                = ["system:masters"]
-  ttl_seconds           = 3600
-  early_renewal_seconds = 300
+  zone        = local.zone
+  cluster_id  = exoscale_sks_cluster.my_sks_cluster.id
+  user        = "kubernetes-admin"
+  groups      = ["system:masters"]
+  ttl_seconds = 3600
 }
+
 resource "local_sensitive_file" "my_sks_kubeconfig_file" {
   filename        = "kubeconfig"
   content         = exoscale_sks_kubeconfig.my_sks_kubeconfig.kubeconfig
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -21,3 +21,15 @@ provider "exoscale" {
   key    = var.exo_key
   secret = var.exo_secret
 }
+
+provider "aws" {
+  region                  = "us-east-1"  # Exoscale nutzt die S3 API, die mit dem AWS Provider kompatibel ist.
+  access_key              = var.exo_key
+  secret_key              = var.exo_secret
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  s3_force_path_style     = true
+  endpoints {
+    s3 = "https://sos-${local.zone}.exo.io"
+  }
+}
