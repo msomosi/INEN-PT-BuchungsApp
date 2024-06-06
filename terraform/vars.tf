2268e604735bf115719b83c0726ddb2a24e4e00b
2310781001 <2310781001@fh-burgenland.at>
2024-06-06 22:16:27 +0200

    update werkzeug# Please enter the commit message for your changes. Lines starting

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,11 +1,11 @@
 variable "exo_secret" {
   type      = string
   sensitive = true
-  default   = "ll1mYK9P5O3xx52P6ftg3vnrFRyNPkdW4PV3oB_NLo8"
+  default   = "goievOEff9BZf67cDjOXAmxpu_3RqkNh0SEIto6TKEE"
 }
 
 variable "exo_key" {
   type      = string
   sensitive = true
-  default   = "EXO5f27ae1ba3685a0d89d9ae58"
+  default   = "EXO481f7d3f65bd795012b23789"
 }
