3842c38d77c717a715197570853630219d818107
Mathias Rangger <mathias.rangger@gmail.com>
2024-10-16 04:03:43 +0200

    Add home route to frontend

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -4,5 +4,13 @@ import os
 app = Flask(__name__)
 app.secret_key = '12345678910111213141516'  # Replace with a strong random value
 
+@app.route('/home')
+def home():
+    if 'google_token' in session:
+        return render_template('homev2.html', user=session['user'])
+    else:
+        return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
