2a4cc1c00f9f11dae390edac8e642dcd7f245c33
Mathias Rangger <mathias.rangger@gmail.com>
2024-11-10 15:50:33 +0100

    Add minikube versions

---
+++ terraform/env/minikube/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    netparse = {
+      source  = "gmeligio/netparse"
+      version = ">= 0.0.3"
+    }
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 1.26.0"
+    }
+  }
+}
