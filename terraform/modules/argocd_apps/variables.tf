9748bc9f3036130170fbf42a4b7f45853fd218cb
seyerjin <matthias.seyer@gmail.com>
2025-01-28 22:52:10 +0000

    Add PostgreSQL application manifest and update ArgoCD configuration

--- terraform/modules/argocd_apps/variables.tf
+++ terraform/modules/argocd_apps/variables.tf
@@ -8,3 +8,15 @@ variable "helm_values" {
   default     = {}
   description = "Additional settings which will be passed to the Helm chart values"
 }
+
+variable "repo_url" {
+  description = "Git repository URL containing the application manifests"
+  type        = string
+  default     = "https://github.com/msomosi/INEN-PT-BuchungsApp.git"
+}
+
+variable "target_revision" {
+  description = "Git branch/tag to use"
+  type        = string
+  default     = "main"
+}
