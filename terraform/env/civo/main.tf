4d4c941b8d3bfee3911082b2a9a425783c184c81
seyerjin <matthias.seyer@gmail.com>
2025-01-29 15:32:16 +0000

    Update PostgreSQL module configuration: set default values for namespace and SQL dump path

--- terraform/env/civo/main.tf
+++ terraform/env/civo/main.tf
@@ -105,7 +105,10 @@ resource "kubernetes_manifest" "app_gateway" {
 
 module "postgres" {
   source = "../../modules/postgres"
-  namespace = var.namespaces[0]  # Or specific namespace
-  sql_dump_path = "${path.module}/init.sql"
+  
+  # These variables are now optional due to defaults
+  namespace = "default"      # Optional, will use "default" if not specified
+  sql_dump_path = "../../modules/postgres/init.sql"
+  
   depends_on = [module.cluster_civo]
 }
