03d899811c10e36bfae5c145fd57805dbe678559
seyerjin <matthias.seyer@gmail.com>
2025-01-30 14:50:19 +0000

    Remove unused resources for CloudNativePG operator and Cert Manager from Terraform configuration

--- terraform/modules/argocd_apps/main.tf
+++ terraform/modules/argocd_apps/main.tf
@@ -19,16 +19,4 @@ resource "helm_release" "argocd_apps" {
   values = [data.utils_deep_merge_yaml.argocd_values.output]
 
 }
-resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  namespace  = "cert-manager"
-  create_namespace = true
-
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-}
 
--- terraform/modules/cloudnativepg_operator/cloudnativepg_operator.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace" "cnpg" {
-  metadata {
-    name = "cnpg-system"
-  }
-}
-
-resource "kubernetes_manifest" "cnpg_operator" {
-  manifest = yamldecode(file("${path.module}/cloudnativepg.yaml"))
-}
